4f24c988e559137459b170929aab10358043a361
DaZaiiiii <90222734+DaZaiiiii@users.noreply.github.com>
2022-06-08 17:15:38 +0300

    Add files via upload

---
+++ Zaytseva.Lab8.Ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zaytseva.Lab8.Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Zaytseva.Lab8.Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zaytseva.Lab8.Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
