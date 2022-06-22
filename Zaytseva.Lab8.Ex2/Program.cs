7d7b8378449d6b18ccb9f57e61f943ae9cc266cc
DaZaiiiii <90222734+DaZaiiiii@users.noreply.github.com>
2022-06-23 02:18:19 +0300

    Update Program.cs

--- Zaytseva.Lab8.Ex2/Program.cs
+++ Zaytseva.Lab8.Ex2/Program.cs
@@ -1,12 +1,64 @@
-﻿using System;
+using System.Text;
+using System.Text.Encodings;
+using System.Text.Encodings.Web;
+using System.Text.Json;
 
-namespace Zaytseva.Lab8.Ex2
+var path = "Ex.2.csv";
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+Encoding encoding = Encoding.GetEncoding(1251);
+
+
+var lines = File.ReadAllLines(path, encoding);
+var Arts = new Art[lines.Length - 1];
+for (var i = 1; i < lines.Length; i++)
 {
-    class Program
+    var splits = lines[i].Split(';');
+    var art = new Art();
+    art.Тип = splits[0];
+    art.Общиеиздержки = Convert.ToInt32(splits[1]);
+    art.Цена = Convert.ToInt32(splits[2]);
+    art.Время = Convert.ToInt32(splits[3]);
+    Arts[i - 1] = art;
+    Console.WriteLine(art);
+}
+
+var result = "result.csv";
+using (StreamWriter streamWriter = new StreamWriter(result, false, encoding))
+{
+    streamWriter.WriteLine($"Type;FC;Price;Time;Profit");
+    for (int i = 0; i < Arts.Length; i++)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        streamWriter.WriteLine(Arts[i].ToExcel());
+    }
+}
+
+var jsonOptions = new JsonSerializerOptions()
+{
+    Encoder = JavaScriptEncoder.Default
+};
+
+var json = JsonSerializer.Serialize(Arts, jsonOptions);
+File.WriteAllText("result.json", json);
+
+var stringJson = File.ReadAllText("result.json");
+var array = JsonSerializer.Deserialize<Art[]>(stringJson);
+
+public class Art
+{
+    public string Тип { get; set; }
+    public int Общиеиздержки { get; set; }
+    public int Цена { get; set; }
+    public int Время { get; set; }
+    public double Доход { get => (Цена * Время) - Общиеиздержки; }
+    public override string ToString()
+    {
+        return $"Тип арта:{Тип} Общие издержки: {Общиеиздержки} Цена: {Цена} Время: {Время} Доход:{Доход}";
+    }
+
+    public string ToExcel()
+    {
+        return $"{Тип};{Общиеиздержки};{Цена};{Время};{Доход}";
     }
 }
